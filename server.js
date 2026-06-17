62af9efbe15301d7cbfbe0278cd1111d712d10be
Montasir Chowdhury Masrol <montasir.masrol@gmail.com>
2026-06-17 19:47:44 +0600

    update

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
 const express=require('express')
 const app=express()
-
+const ok=s=>/^[1-9]\d*$/.test(s)
 const gcd=(a,b)=>b?gcd(b,a%b):a
-
-app.get('/montasir_masrol_gmail_com',(req,res)=>{
-  let{x,y}=req.query
-
-  if(!/^[1-9]\d*$/.test(x)||!/^[1-9]\d*$/.test(y))
-    return res.send('NaN')
-
-  x=BigInt(x)
-  y=BigInt(y)
-
-  res.send(((x*y)/BigInt(gcd(Number(x),Number(y)))).toString())
+app.get('/montasir_masrol_gmail_com',(q,r)=>{
+ let{x,y}=q.query
+ r.type('text/plain')
+ if(!ok(x)||!ok(y))return r.send('NaN')
+ x=BigInt(x);y=BigInt(y)
+ r.send((x/gcd(x,y)*y).toString())
 })
-
 app.listen(process.env.PORT||3000)
